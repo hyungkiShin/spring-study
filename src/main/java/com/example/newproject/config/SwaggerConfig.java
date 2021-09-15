1b4b831472e21f544949d7f135dc1fcb013a320f
hyungkiShin <tkaqkeldk@nate.com>
2021-09-15 22:40:19 +0900

    [Swagger-Ui] 적용

---
+++ src/main/java/com/example/newproject/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.example.newproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+
+    }
+
+    private ApiInfo apiInfo() {
+        String description = "Welcome Log Company";
+        return new ApiInfoBuilder()
+                .title("SWAGGER TEST")
+                .description(description)
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/com/example/newproject/user/UserController.java
+++ src/main/java/com/example/newproject/user/UserController.java
@@ -1,12 +1,22 @@
 package com.example.newproject.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -17,18 +27,47 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public List<User> retriveAllUsers() {
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
+//    @GetMapping("/users/{id}")
+//    public ResponseEntity<EntityModel<User>> retrieveUser(@PathVariable int id) {
+//        User user = service.findOne(id);
+//
+//        if (user == null) {
+//            throw new UserNotFoundException("id-" + id);
+//        }
+//
+//        EntityModel entityModel = EntityModel.of(user);
+//
+//        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+//        entityModel.add(linkTo.withRel("all-users"));
+//        return ResponseEntity.ok(entityModel);
+//    }
+
     @GetMapping("/users/{id}")
-    public User retriveUser(@PathVariable int id) {
+    public ResponseEntity<EntityModel<User>> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
-        if(user == null) {
-            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+
+        if (user == null) {
+            throw new UserNotFoundException("id-" + id);
         }
-        return user;
+//        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+//                .filterOutAllExcept("id", "name", "joinDate", "ssn");
+//
+//        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+//        MappingJacksonValue mapping = new MappingJacksonValue(user);
+//        mapping.setFilters(filters);
+
+        EntityModel entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkTo.withRel("all-users"));
+
+        return ResponseEntity.ok(entityModel);
     }
+
 
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
