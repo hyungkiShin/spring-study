3dac05adca26e056bd1c1ae6b032fb912b1918b7
hyungkiShin <tkaqkeldk@nate.com>
2021-09-16 00:44:15 +0900

    [java monitoring] dependency 추가

--- src/main/java/com/example/newproject/config/SwaggerConfig.java
+++ src/main/java/com/example/newproject/config/SwaggerConfig.java
@@ -10,25 +10,40 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final Contact DEFAULT_CONCAT = new Contact("hyungki", "www.naver.com", "www.naver.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title",
+            "My User management REST API service",
+            "1.0",
+            "urn:tos",
+            DEFAULT_CONCAT,
+            "Apache 2.0",
+            "http://www.apache.org/licences/LICENSE-2.0", new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
     @Bean
     public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.any())
+//                .paths(PathSelectors.any())
+//                .build().apiInfo(apiInfo());
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build().apiInfo(apiInfo());
-
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 
     private ApiInfo apiInfo() {
